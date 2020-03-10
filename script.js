14ce5c77dc5ac83e71c2dd3feb20e307601a76b5
Sal Castoro <salcastoro@gmail.com>
2020-03-10 15:59:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+
+  if (username.value === '') {
+    showError(email, 'email is required');
+  } else {
+    showSuccess(email);
+  }
+});
